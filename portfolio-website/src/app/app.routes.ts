84392a1942f249dedb69f2904f5a275ac54f0b2f
ShubhamPrasad-2711 <shubhamprasad2711@gmail.com>
2025-01-01 23:21:27 +0530

    test commit

--- portfolio-website/src/app/app.routes.ts
+++ portfolio-website/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  {
+  { 
     path: 'home',
     loadComponent: () => import('./main/main.component')
       .then(m => m.MainComponent)
