285ce6ba72d7fc38e8c3ffef4b20e60bfba7c279
ShubhamPrasad-2711 <shubhamprasad2711@gmail.com>
2025-01-01 17:36:36 +0530

    work exp and certifications done

--- portfolio-website/src/app/certifications/certifications.component.ts
+++ portfolio-website/src/app/certifications/certifications.component.ts
@@ -1,12 +1,89 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+
+interface Certificate {
+  name: string;
+  issuer: string;
+  date: string;
+  fileName: string;
+  iconPath: string;
+}
 
 @Component({
   selector: 'app-certifications',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './certifications.component.html',
   styleUrl: './certifications.component.scss'
 })
 export class CertificationsComponent {
+  certificates: Certificate[] = [
+    {
+      name: "Introduction to DevOps",
+      issuer: "IBM with Coursera",
+      date: "Dec 2024",
+      fileName: "Intro-DevOps.pdf",
+      iconPath: "../../assets/icons/coursera-icon.png"
+    },
+    {
+      name: "Web Design",
+      issuer: "Udemy",
+      date: "May 2021",
+      fileName: "Web Design.pdf",
+      iconPath: "../../assets/icons/udemy-icon.png"
+    },
+    {
+      name: "Introduction to Agile Development and Scrum",
+      issuer: "IBM with Coursera",
+      date: "Dec 2024",
+      fileName: "Intro-to-Scrum.pdf",
+      iconPath: "../../assets/icons/coursera-icon.png"
+    },
+    {
+      name: "Python - Complete Course",
+      issuer: "Udemy",
+      date: "Oct 2020",
+      fileName: "Certificate - PYTHON.pdf",
+       iconPath: "../../assets/icons/udemy-icon.png"
+    },
+    {
+      name: "ReactJS - Basic to Advanced Development",
+      issuer: "Udemy",
+      date: "Dec 2022",
+      fileName: "ReactJS.pdf",
+       iconPath: "../../assets/icons/udemy-icon.png"
+    },
+    {
+      name: "Graphic Designing and tools",
+      issuer: "Udemy",
+      date: "July 2021",
+      fileName: "Graphic Designing Certificate.pdf",
+       iconPath: "../../assets/icons/udemy-icon.png"
+    },
+    {
+      name: "Soft skills Certification",
+      issuer: "Barclays",
+      date: "Dec 2021",
+      fileName: "soft-skills.pdf",
+       iconPath: "../../assets/icons/barclays-icon.png"
+    },
+    {
+      name: "HTML - Basic to Advance",
+      issuer: "Udemy",
+      date: "Nov 2020",
+      fileName: "Certificate - HTML.pdf",
+       iconPath: "../../assets/icons/udemy-icon.png"
+    },
+    {
+      name: "Programming in C",
+      issuer: "Udemy",
+      date: "Sept 2020",
+      fileName: "Certificate - C.pdf",
+       iconPath: "../../assets/icons/udemy-icon.png"
+    }
+  ];
 
+  viewCertificate(fileName: string): void {
+    window.open(`../../assets/certificates/${fileName}`, '_blank');
+  }
 }
--- portfolio-website/src/app/work-experience/work-experience.component.ts
+++ portfolio-website/src/app/work-experience/work-experience.component.ts
@@ -1,12 +1,51 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+
+interface WorkExperience {
+  company: string;
+  role: string;
+  duration: string;
+  description: string[];
+  technologies: string[];
+}
 
 @Component({
   selector: 'app-work-experience',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './work-experience.component.html',
   styleUrl: './work-experience.component.scss'
 })
 export class WorkExperienceComponent {
+  experiences: WorkExperience[] = [
+    {
+      company: "Transmogriffy Global Private Limited",
+      role: "Fullstack Software Developer",
+      duration: "Jan 2024 - Aug 2024",
+      description: [
+        "Led development of scalable web applications using Angular and Node.js",
+        "Led a team of 2 interns on 2+ full-stack projects, ensuring quality and deadlines.",
+        "Implemented CI/CD pipelines for automated deployment",
+        "Improved system performance by 40% through optimization techniques",
+        "Developed RESTful APIs using NestJS framework",
+        "Managed AWS cloud infrastructure for application hosting"
+      ],
+      technologies: ["Angular", "Ionic", "NestJS", "Docker",  "Javascript", "TypeScript", "AWS"]
+    },
+    {
+      company: "Vara Infrovate Private Limited",
+      role: "Software Developer",
+      duration: "Jan 2023 - Jan 2024",
+      description: [
+        "Designed and developed responsive web interfaces using Angular , Angular Material and NgRx components.",
+        "Contributed to a blockchain development team, exploring decentralized applications.",
+        "Collaborated with UX team to implement modern design patterns",
+        "Proficient in Git, GitHub, JIRA, and collaborative tools for efficient project management.",
+        "Implemented complex styling using SCSS",
+        "Ensured cross-browser compatibility across all major browsers"
+      ],
+      technologies: ["Angular", "TypeScript", "Angular Material", "NgRx"]
+    }
+  ];
 
 }
